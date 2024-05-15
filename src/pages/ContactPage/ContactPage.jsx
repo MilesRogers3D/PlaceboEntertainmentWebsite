f94542f4a768ed9e0180b1cb74767fefec09e1ba
Alec Pizziferro <alecpizz@gmail.com>
2024-05-15 18:45:34 -0500

    contact form mail routing

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -3,15 +3,16 @@ export const ContactPage = () => {
      return (
           <div className="contact-page">
                <h1 className="contact-title">GET CONNECTED!</h1>
-               <form className="contact-container">
+               <form className="contact-container" action="https://formsubmit.co/4922688420abc1562fe4f7eaeb35256b" method="POST">
                     <label className="contact-label">Name*</label>
-                    <input className="contact-input" type="text"></input>
+                    <input className="contact-input" type="text" name="name" required></input>
 
                     <label className="contact-label">Email*</label>
-                    <input className="contact-input" type="text"></input>
+                    <input className="contact-input" type="text" name="email" required></input>
 
+                    <input type="hidden" name="_next" value="https://placeboentertainment.com"></input>
                     <label className="contact-label">Message*</label>
-                    <textarea className="contact-input-message"></textarea>
+                    <textarea className="contact-input-message" name="message"></textarea>
                     <input type="submit" value="Submit" className="contact-submit"></input>
                </form>
           </div>
