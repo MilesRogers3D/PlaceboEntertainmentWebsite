59cae62db24dbfc858e4864956f1c682552d5dd0
Alec Pizziferro <alecpizz@gmail.com>
2024-06-20 21:58:33 -0500

    reformat site to use markdown for pogs

--- src/pages/BlogEntryPage/BlogEntry.jsx
+++ src/pages/BlogEntryPage/BlogEntry.jsx
@@ -5,28 +5,55 @@ import { useState, useEffect } from "react";
 import LoadingPage from "../LoadingPage/LoadingPage";
 import ReactImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
-
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
 export const BlogEntry = () => {
 
-     const [retrievedData, setRetrievedData] = useState('');
+     const [retrievedData, setRetrievedData] = useState();
      const [toBlogPage, setToBlogPage] = useState(false);
      const [dataLoaded, setDataLoaded] = useState(false);
+     const [foundMDPage, setFoundMDPage] = useState('');
 
      const getData = () => {
-          fetch('blogposts.json', {
-               headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-               }
-          }).then(function (response) {
-               return response.json();
-          }).then(function (myJson) {
-               if (date.blogID == 0) return;
+          const dataFetch = async () => {
+               const data = await (
+                    await fetch('blogposts.json', {
+                         headers: {
+                              'Content-Type': 'application/json',
+                              'Accept': 'application/json'
+                         }
+                    })
+               ).json();
+
+               setRetrievedData(data["blog_entries"][date.blogID]);
+               let entryPage = data["blog_entries"][date.blogID].entry_file;
+               const markdown = (
+                    await fetch(entryPage).then(response => response.text()).then(result => setFoundMDPage(result))
+               );
+          
                setDataLoaded(true);
-               setRetrievedData(myJson["blog_entries"][date.blogID]);
-               console.log(retrievedData.images);
-          })
+               // setDataLoaded(true);
+          }
+
+          dataFetch();
+          // console.log(retrievedData);
+          // setDataLoaded(true);
+          // setRetrievedData(dataFetch());
+          // // console.log(dataFetch["blog_entries"]);
+          // fetch('blogposts.json', {
+          //      headers: {
+          //           'Content-Type': 'application/json',
+          //           'Accept': 'application/json'
+          //      }
+          // }).then(function (response) {
+          //      return response.json();
+          // }).then(function (myJson) {
+          //      if (date.blogID == 0) return;
+          //      setDataLoaded(true);
+          //      setRetrievedData(myJson["blog_entries"][date.blogID]);
+          // })
      }
 
      let date = useParams();
@@ -35,7 +62,7 @@ export const BlogEntry = () => {
           getData();
      }, [])
 
-     
+
      const buildGallery = () => {
           if (retrievedData.images.length === 0) {
                return <></>
@@ -44,21 +71,19 @@ export const BlogEntry = () => {
           retrievedData.images.forEach(element => {
                result.push({ original: element });
           });
-          return <ReactImageGallery items={result}/>
+          return <ReactImageGallery items={result} />
      }
 
-     if(!dataLoaded){
-          return <LoadingPage/>;
+     if (!dataLoaded) {
+          return <LoadingPage />;
      }
-
 
      if (toBlogPage === true) {
           return <Navigate to="/blog" />;
      }
      return (
           <div className="blog-entry">
-               <h1 className="blog-entry-date">{retrievedData.title}</h1>
-               <p className="blog-entry-body">{retrievedData.body}</p>
+               <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{foundMDPage}</Markdown>
                <div>
                     {buildGallery()}
                </div>
