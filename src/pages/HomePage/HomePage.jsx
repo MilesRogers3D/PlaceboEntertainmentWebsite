277d7d617df06bc3b316a34b8136387333863bd9
Alec Pizziferro <alecpizz@gmail.com>
2024-06-27 23:40:40 -0500

    home page style fixes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,8 @@ import { Container } from "react-bootstrap";
 
 export const HomePage = () => {
      return (
-          <div className="home-container">
-               <div className="home-outer">
+          <div className="home-outer">
+               <div className="home-container">
                     <h1 className="home-title">WE ARE PLACEBO <br></br>ENTERTAINMENT</h1>
                     <p className="home-slogan">What started as an idea between friends became an indie game studio. We are dedicated to weaving story and style together in new, compelling ways. </p>
                </div>
