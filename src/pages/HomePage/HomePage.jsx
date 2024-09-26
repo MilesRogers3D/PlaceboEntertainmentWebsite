d43397c670cc2a5c9d6d435394ff55a12b78e588
Alec Pizziferro <alecpizz@gmail.com>
2024-09-25 20:20:26 -0500

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -12,7 +12,6 @@ export const HomePage = () => {
                <div className="home-container">
                     <h1 className="home-title">WE ARE PLACEBO <br></br>ENTERTAINMENT</h1>
                     <p className="home-slogan">What started as an idea between friends became an indie game studio. We are dedicated to weaving story and style together in new, compelling ways. </p>
-                    <button className="wishlist-button" onClick={buttonOnClick}>Play Beyond the Marion Now!</button>
                </div>
           </div>
 
